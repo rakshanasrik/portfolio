a646db01d4afc27523267cf5c662976001ae8f5c
Rakshana Sri K <142480823+rakshanasrik@users.noreply.github.com>
2024-10-27 21:00:30 +0530

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,30 @@
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Nanotechnology" , "Web Development","VLSI","Digital Electronics"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+/*
+function send(){
+    const form = document.getElementById("form");
+    let nam = document.forms["form"]["name"].value;
+    let mail = document.forms["form"]["mail"].value;
+    let sub = document.forms["form"]["sub"].value;
+    let msg = document.forms["form"]["msg"].value;
+    console.log(nam);
+}*/
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+    
